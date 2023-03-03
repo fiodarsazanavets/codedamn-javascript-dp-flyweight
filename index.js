26f90683c4edcb7adefe4b43d4534027765d8b88
Fiodar Sazanavets <33926695+fiodarsazanavets@users.noreply.github.com>
2023-03-03 18:33:18 +0800

    Refactor so it can be used as an exercise.

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ var army = { };
 
 console.log('Adding militiamen to the army.');
 for (var i = 0; i < 1000; i++) {
-    army[i] = factory.getFlyweight('Militiaman');
+    army[i] = new SoldierFlyweight('Militiaman', 1, 1, 1);
 }
 
 console.log('Adding soldiers to the army.');
 for (var i = 0; i < 100; i++) {
-    army[i + 1000] = factory.getFlyweight('Soldier');
+    army[i + 1000] = new SoldierFlyweight('Soldier', 2, 1, 2);
 }
 
 console.log('Adding knights to the army.');
 for (var i = 0; i < 10; i++) {
-    army[i + 1100] = factory.getFlyweight('Knight');
+    army[i + 1100] = new SoldierFlyweight('Soldier', 10, 5, 5);
 }
 
 console.log(`Total army size is ${Object.keys(army).length}.`);
